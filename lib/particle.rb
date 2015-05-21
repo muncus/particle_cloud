75e2ba7b39146329e130f7458ccc22e6441c09d9
Marc Dougherty <muncus@gmail.com>
2015-05-20 23:30:00 -0700

    start of Particle api - device listing

--- lib/particle.rb
+++ lib/particle.rb
@@ -1,6 +1,26 @@
+require 'faraday'
+require 'json'
+
 module Particle
 
-  class User
+  DefaultBaseUrl = "https://api.particle.io/v1/"
+
+  class Client
+    def initialize(access_token, base_url=nil)
+      @access_token = access_token
+      @client = Faraday.new(:url => (base_url or DefaultBaseUrl),
+                            :params => {access_token: @access_token})
+    end
+
+    def devices()
+      r = @client.get('devices')
+      if r.success?
+        @device_list = JSON.parse(r.body)
+      else
+        raise
+      end
+    end
+      
   end
 
 end
