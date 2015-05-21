d544a1f74f3c604ae108c1eb3a18fe4e18536a9c
Marc Dougherty <muncus@gmail.com>
2015-05-21 14:03:44 -0700

    only record new episodes!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/testdata/vcr_cassettes'
   c.hook_into :webmock
   c.default_cassette_options = {
-    :record                 => :once,
+    :record                 => :new_episodes,
     :allow_playback_repeats => true,
   }
   c.allow_http_connections_when_no_cassette = false
