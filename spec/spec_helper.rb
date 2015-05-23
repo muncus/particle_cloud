23f816dfd14b824e7cca988d8f21cc3e13e67e87
Marc Dougherty <muncus@gmail.com>
2015-05-23 12:59:11 -0700

    add sanitized cassette for access tokens, switch to :once for recording.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/testdata/vcr_cassettes'
   c.hook_into :webmock
   c.default_cassette_options = {
-    :record                 => :new_episodes,
+    :record                 => :once,
     :allow_playback_repeats => true,
   }
   c.allow_http_connections_when_no_cassette = false
